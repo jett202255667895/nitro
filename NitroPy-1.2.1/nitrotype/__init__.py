cb5530e0e4c1ce16e9c5ce57cc8db53e232ccdf5
jett202255667895 <141539577+jett202255667895@users.noreply.github.com>
2024-04-23 10:45:52 +0930

    Delete NitroPy-1.2.1 directory

--- NitroPy-1.2.1/nitrotype/__init__.py
+++
@@ -1,5 +0,0 @@
-from ._nitrotype import *
-from ._nitrotypeteam import NitrotypeTeam
-from ._news import NitrotypeNews
-from ._nitromath import NitromathRacer
-from ._nitromathteam import NitromathTeam
